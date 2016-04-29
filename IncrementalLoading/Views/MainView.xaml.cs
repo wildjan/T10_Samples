5332fb0a3f49f986d34ce73b99898a3d50890559
janwi <jan.wild@hotmail.com>
2016-04-29 09:01:01 +0200

    Consolidated nuget packages

---
+++ IncrementalLoading/Views/MainView.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Messaging.ViewModels;
+using Windows.UI.Xaml.Controls;
+
+namespace Messaging.Views
+{
+    public sealed partial class MainView : Page
+    {
+        public MainView()
+        {
+            this.InitializeComponent();
+        }
+
+        public MainViewModel ViewModel
+        {
+            get
+            {
+                return this.DataContext as MainViewModel;
+            }
+        }
+    }
+}
